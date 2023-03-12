f3bc3f62998ffe698b89de08113a68a379b0ff27
Christian Gomes <123382397+Christian-CGBS@users.noreply.github.com>
2023-03-12 14:30:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var app = require('./config/server');
 
 require("dotenv").config();
 
-app.listen(3000, function(){
+/* app.listen(3000, function(){
     console.log('servidor ON');
-});
+}); */
 
-/* para o deploy da aplicação
+/* para o deploy da aplicação */
 app.listen({
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(() => {
-    console.log('servidor está ativo')
+    console.log('servidor está ativo!')
 })
-*/
