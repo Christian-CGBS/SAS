78d1943ed2eeff6f15553baf6762cc8e85bf74a6
Christian Gomes <123382397+Christian-CGBS@users.noreply.github.com>
2023-08-08 19:07:43 -0300

    Gerar planilha CSV

--- app/views/saida-scripts.js
+++ app/views/saida-scripts.js
@@ -5,3 +5,21 @@ button.addEventListener("click", () => {
   const doc = new jsPDF("p", "pt", "a4");
   doc.addHTML(document.body, () => doc.save("html.pdf"));
 });
+
+// instrução para gerar uma planilha CSV com as respostas dos formulários
+
+buttonCsv.addEventListener("click", () => {
+  console.log('imprimindo planilha');
+  const sampleData = [ questoes[i] ];
+  const csv = Papa.unparse(sampleData);
+  const blob = new Blob([csv], {type:"text/csv"});
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.ref = url;
+  a.download = "sample_data.csv";
+  a.textContent = "Download CSV";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+});
